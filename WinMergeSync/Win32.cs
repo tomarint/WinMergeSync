54dec6c061951809448d60d72dddb5335a6e65d4
tomarin <tomarint@gmail.com>
2019-02-14 15:37:56 +0900

    Initial source commit.

---
+++ WinMergeSync/Win32.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WinMergeSync {
+    public static class Win32 {
+        //---------------------------------------------------------------
+        // EnumWindows
+        //---------------------------------------------------------------
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool EnumWindows(WNDENUMPROC lpEnumFunc, IntPtr lParam);
+
+        delegate bool WNDENUMPROC(IntPtr hWnd, IntPtr lParam);
+
+        static bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam) {
+            return Win32.enumWindowsCallback(hWnd);
+        }
+
+        static Func<IntPtr, bool> enumWindowsCallback;
+
+        public static bool EnumWindowsEach(Func<IntPtr, bool> enumWindowsCallback) {
+            Win32.enumWindowsCallback = enumWindowsCallback;
+            return Win32.EnumWindows(EnumWindowsProc, IntPtr.Zero);
+        }
+
+        //---------------------------------------------------------------
+        // GetClassName
+        //---------------------------------------------------------------
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        public static string GetClassNameWrapper(IntPtr hWnd) {
+            StringBuilder className = new StringBuilder(1000);
+            int nRet = Win32.GetClassName(hWnd, className, className.Capacity);
+            if (nRet != 0) {
+                return className.ToString();
+            }
+            else {
+                return null;
+            }
+        }
+
+        //---------------------------------------------------------------
+        // FindWindowEx
+        //---------------------------------------------------------------
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        //---------------------------------------------------------------
+        // SendMessage / PostMessage
+        //---------------------------------------------------------------
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, UIntPtr wParam, UIntPtr lParam);
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool PostMessage(IntPtr hWnd, uint Msg, UIntPtr wParam, UIntPtr lParam);
+
+        public static uint WM_KEYDOWN = 0x0100;
+        public static uint WM_KEYUP = 0x0101;
+        public static uint WM_CHAR = 0x0102;
+        public static uint WM_SYSKEYDOWN = 0x0104;
+        public static uint WM_SYSKEYUP = 0x0105;
+        public static uint WM_COMMAND = 0x0111;
+        public static uint WM_MOUSEWHEEL = 0x020A;
+
+        public static uint VK_MENU = 0x12;
+        public static uint VK_DOWN = 0x28;
+    }
+}
---
+++ WinMergeSync/WindowMain.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace WinMergeSync {
+    /// <summary>
+    /// Interaction logic for WindowMain.xaml
+    /// </summary>
+    public partial class WindowMain : Window {
+        public WindowMain() {
+            InitializeComponent();
+        }
+
+        private IEnumerable<IntPtr> WinMergeWindows(bool child) {
+            foreach (Process p in Process.GetProcesses()) {
+                IntPtr hWnd = p.MainWindowHandle;
+                if (hWnd == IntPtr.Zero) {
+                    continue;
+                }
+                string className = Win32.GetClassNameWrapper(hWnd);
+                if (className != "WinMergeWindowClassW") {
+                    continue;
+                }
+                /*
+                if (child) {
+                    hWnd = Win32.FindWindowEx(hWnd, IntPtr.Zero, "MDIClient", null);
+                    if (hWnd == IntPtr.Zero) {
+                        continue;
+                    }
+                    hWnd = Win32.FindWindowEx(hWnd, IntPtr.Zero, "Afx:00400000:b:00010013:00000006:00020D41", null);
+                    if (hWnd == IntPtr.Zero) {
+                        continue;
+                    }
+                    hWnd = Win32.FindWindowEx(hWnd, IntPtr.Zero, "AfxMDIFrame100u", null);
+                    if (hWnd == IntPtr.Zero) {
+                        continue;
+                    }
+                }
+                */
+                yield return hWnd;
+            }
+        }
+
+        private void ButtonPrev_Click(object sender, RoutedEventArgs e) {
+            foreach (IntPtr hWnd in this.WinMergeWindows(false)) {
+                Win32.SendMessage(hWnd, Win32.WM_COMMAND, new UIntPtr(0x18040), new UIntPtr(0));
+            }
+        }
+        private void ButtonNext_Click(object sender, RoutedEventArgs e) {
+            foreach (IntPtr hWnd in this.WinMergeWindows(false)) {
+                Win32.SendMessage(hWnd, Win32.WM_COMMAND, new UIntPtr(0x18041), new UIntPtr(0));
+            }
+        }
+    }
+}
